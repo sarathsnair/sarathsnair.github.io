264909a395db07d43f5889b61e96e82056c3dc48
Sarath S Nair <Sarath S Nair>
2017-05-21 20:20:22 +0530

    modified deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,66 +1,21 @@
 #!/bin/bash
-set -e
+set -e # exit with nonzero exit code if anything fails
 
-# Deploy built docs to this branch
-TARGET_BRANCH=master
- 
-if [ ! -d "$SOURCE_DIR" ]; then
-  echo "SOURCE_DIR ($SOURCE_DIR) does not exist, build the source directory before deploying"
-  exit 1
-fi
- 
-REPO=$(git config remote.origin.url)
- 
-if [ -n "$TRAVIS_BUILD_ID" ]; then
-  # When running on Travis we need to use SSH to deploy to GitHub
-  #
-  # The following converts the repo URL to an SSH location,
-  # decrypts the SSH key and sets up the Git config with
-  # the correct user name and email (globally as this is a
-  # temporary travis environment)
-  #
-  # Set the following environment variables in the travis configuration (.travis.yml)
-  #
-  #   DEPLOY_BRANCH    - The only branch that Travis should deploy from
-  #   ENCRYPTION_LABEL - The label assigned when encrypting the SSH key using travis encrypt-file
-  #   GIT_NAME         - The Git user name
-  #   GIT_EMAIL        - The Git user email
-  #
-  echo DEPLOY_BRANCH: $DEPLOY_BRANCH
-  echo ENCRYPTION_LABEL: $ENCRYPTION_LABEL
-  echo GIT_NAME: $GIT_NAME
-  echo GIT_EMAIL: $GIT_EMAIL
-  if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
-    echo "Travis should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
-    exit 0
-  else
-    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-      echo "Travis should not deploy from pull requests"
-      exit 0
-    else
-      # switch both git and https protocols as we don't know which travis
-      # is using today (it changed!)
-      REPO=${REPO/git:\/\/github.com\//git@github.com:}
-      REPO=${REPO/https:\/\/github.com\//git@github.com:}
-      
-    #   chmod 600 $SSH_KEY
-    #   eval `ssh-agent -s`
-    #   ssh-add $SSH_KEY
-      git config --global user.name "$GIT_NAME"
-      git config --global user.email "$GIT_EMAIL"
-    fi
-  fi
-fi
- 
-REPO_NAME=$(basename $REPO)
-TARGET_DIR=$(mktemp -d /tmp/$REPO_NAME.XXXX)
-REV=$(git rev-parse HEAD)
-git clone --branch ${TARGET_BRANCH} ${REPO} ${TARGET_DIR}
-rsync -rt --delete --exclude=".git" --exclude=".travis.yml" $SOURCE_DIR/ $TARGET_DIR/
-cd $TARGET_DIR
-git add -A .
-git commit --allow-empty -m "Built from commit $REV"
-git push $REPO $TARGET_BRANCH
+# go to the build directory and create a *new* Git repo
+cd build
+git init
 
-git config --global user.name "$GIT_NAME"
-git config --global user.email "$GIT_EMAIL"
+# inside this git repo we'll pretend to be a new user
+git config user.name "Travis CI"
+git config user.email "sarathsnairxyz@gmail.com"
+
+# The first and only commit to this new Git repo contains all the
+# files present with the commit message "Deploy to GitHub Pages".
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+# Force push from the current repo's master branch to the remote
+# repo's gh-pages branch. (All previous history on the gh-pages branch
+# will be lost, since we are overwriting it.) We redirect any output to
+# /dev/null to hide any sensitive credential data that might otherwise be exposed.
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" source:master
