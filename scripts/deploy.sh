397e34b1028f5c07126893b6820035de269f2988
Sarath S Nair <Sarath S Nair>
2017-05-21 21:00:57 +0530

    Display commit ID in commit message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,7 +15,7 @@ git config user.email "sarathsnairxyz@gmail.com"
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
-git commit -m "Deploy to GitHub Pages"
+git commit -m "Deploy to GitHub Pages - Commit ID - $TRAVIS_COMMIT"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
