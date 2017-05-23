ef1d05074dfd80cdc2bea6953d9869678970f077
Sarath S Nair <Sarath S Nair>
2017-05-24 00:00:37 +0530

    Modified grunt file to fix bootstrap issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function (grunt) {
             },
             html: {
                 cwd: 'websrc',
-                src: ['assets/files/**', 'assets/fonts/**','index.html'],
+                src: ['assets/files/**', 'assets/fonts/**', 'index.html'],
                 dest: 'build',
                 expand: true
             }
@@ -58,6 +58,7 @@ module.exports = function (grunt) {
                 files: {
                     'build/assets/css/style.min.css': [
                         'websrc/assets/css/font-awesome.min.css',
+                        'websrc/assets/css/bootstrap.min.css',
                         'websrc/assets/css/main.css',
                         'websrc/assets/css/custom.css',
                         'websrc/assets/css/highlight.css',
